42f47cbb744321b1a9085e4ac08ecd747b1696c0
kylehovey <khoveyc@gmail.com>
2018-01-05 15:41:16 -0700

    Added API stub.

--- app_modules/express/routes/api.js
+++ app_modules/express/routes/api.js
@@ -1,15 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET home page. */
-router.get("/", (req, res, next) => {
-  // Respond
-  res.json({
-    success : true,
-    results : "Hello World!"
-  });
-
-  next();
-});
+/**
+ * API Code here
+ * TODO
+ */
 
 module.exports = router;
